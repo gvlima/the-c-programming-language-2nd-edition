8537d40cf7c651b970a7603f16b1225e585e7cdd
Gustavo Lima <gustavo.volima@gmail.com>
2021-02-23 01:59:57 -0300

    add new exercises 5-12 - 5-20.

---
+++ Chapter 05/5-18/main.c
@@ -0,0 +1,12 @@
+/**
+ * Chapter: 5
+ * Exercise: 5-16 - Add -d ("directory order") option, which makes comparisons only on letters, numbers and blanks. Make sure
+ * it works in conjunction with -f.
+ **/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
